0aa1ce6722b8d132eac3d7c4f8052791f28b7e5d
RicciCEV <ricardo_christmann_apps1oa1920@cev.com>
2020-11-29 18:10:44 +0100

    added registerForm and disabled sign in btn if required fields are not completed

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,6 @@
 import { RegisterForm } from './../../models/registerForm';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-register',   // nombre del componente.
@@ -7,11 +8,26 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./register.component.css']     // fichero css del componente.
 })
 export class RegisterComponent implements OnInit {
+    // variable instancia de clase RegisterForm.
     public registerModel: RegisterForm;
+    public ok: boolean;
+    public errorMsg: string;
 
     constructor() { 
         this.registerModel = new RegisterForm();
+        this.ok = true;
+        this.errorMsg = '';
     }
 
     ngOnInit(): void { }
+
+    onSubmit(f: NgForm): void {
+        if(this.registerModel.passw.length < 6) {
+            this.ok = false;
+            this.errorMsg = 'La password debe contener almenos 6 caracteres';
+        } else {
+            this.ok = true;
+        }
+
+    }
 }
